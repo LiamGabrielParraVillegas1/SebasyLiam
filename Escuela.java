a6d69114b4ca5a66e0a4cf0b05a478f8fa829987
VelazquezMartinezSebastian <20253rd029@utez.edu.mx>
2026-03-04 12:48:27 -0600

    Agregue los atributos clase Escuela

---
+++ Escuela.java
@@ -0,0 +1,11 @@
+public class Escuela {
+
+    private String alumno1;
+    private String alumno2;
+    private String alumno3;
+    private String alumno4;
+    private String alumno5;
+
+    private int contador = 0;
+    
+}
